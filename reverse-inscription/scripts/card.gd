f4a5f34a66c52de6b476782182cd265c0aa53006
drunken_barrel <joedell0@outlook.com>
2026-09-12 17:17:27 +1000

    animation added
    It's slow and a little clunky but combat is now readable

--- reverse-inscription/scripts/card.gd
+++ reverse-inscription/scripts/card.gd
@@ -2,8 +2,10 @@ extends Control
 
 # pre-load all directories
 @onready var CardName: Label = $CardName
+@onready var CardArt: Sprite2D = $CardArt
 @onready var Health: Label = $Health
 @onready var Strength: Label = $Strength
+@onready var  PlayAnimation: AnimationPlayer = $AnimationPlayer
 
 # variables
 @export var card_name: String = "filler":
@@ -43,9 +45,9 @@ extends Control
 		strength = value
 		# update display
 		if Strength != null:
-			Strength.text = "HP: " + str(strength)
+			Strength.text = "STR: " + str(strength)
 		else:
-			$Strength.text = "HP: " + str(strength)
+			$Strength.text = "STR: " + str(strength)
 @export var ability: String = "empty":
 	set(value):
 		ability = value
@@ -54,12 +56,19 @@ extends Control
 func _ready() -> void:
 	PvpManager.begin_combat_signal.connect(attack_if_first)
 	PvpManager.output_attack_signal.connect(take_damage)
+	if side == 1:
+		CardArt.flip_v = true
 
 func attack_if_first() -> void:
 	if side == 0:
 		attack()
 
 func attack() -> void:
+	if side == 0:
+		PlayAnimation.play("attack_up")
+	else:
+		PlayAnimation.play("attack_down")
+	await get_tree().create_timer(0.5).timeout
 	PvpManager.output_attack_signal.emit(lane,side,strength)
 
 func take_damage(attacker_lane: int,attacker_side: int,attacker_strength: int) -> void:
