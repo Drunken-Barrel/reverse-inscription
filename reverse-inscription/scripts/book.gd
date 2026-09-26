3f9667053bcd735dac095256e4b07920efd5c480
drunken_barrel <joedell0@outlook.com>
2026-09-26 14:05:49 +1000

    add book with placeholders
    Doesn't do much on anything yet; committing this because I have some stuff to clean up that I want to isolate to a single commit

---
+++ reverse-inscription/scripts/book.gd
@@ -0,0 +1,21 @@
+extends Control
+
+# variables
+var dragging = false
+var cursor_offset = Vector2.ZERO
+
+func _gui_input(event: InputEvent) -> void:
+	# check if the input was a left click
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		# check if it was just pressed
+		if event.pressed:
+			# set dragging to true and record the mouse offset
+			dragging = true
+			cursor_offset = global_position - get_global_mouse_position()
+		else:
+			dragging = false
+	# follow the mouse if it moves
+	if event is InputEventMouseMotion and dragging:
+		global_position = get_global_mouse_position() + cursor_offset
+	# consume the input
+	accept_event()
